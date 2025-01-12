b95aa0bd888153c5751c13fd158e819b2462692f
Inna-A-R <inna1990sweet@mail.ru>
2025-01-12 19:45:51 +0300

    test

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-    int n;
-    for (n = 0; n < 10; n++)
-            printf("%d %d\n", n, 2 * n + 1);
+    const int FIRST_OZ = 46;
+    const int NEXT_OZ = 20;
+    int ounces, cost;
+    printf("ounces cost\n");
+    for (ounces = 1, cost = FIRST_OZ; ounces <= 16; ounces++, cost = cost + NEXT_OZ)
+            printf("%3d $%6.2f\n", ounces, cost/100.0);
     return 0;
 }
